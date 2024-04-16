863e2cbcc2ec9f80523def0888be3e1d71b08270
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2024-04-16 12:34:53 +0200

    backend description duplicate commented

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-250720241"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    workspace_key_prefix = "tf"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-state-250720241"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#     workspace_key_prefix = "tf"
+#   }
+# }
