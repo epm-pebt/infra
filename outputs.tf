fad51ab685c55533e1b9c514fed856c93c6673fe
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2024-06-30 17:33:09 +0200

    changed to fit CI-CD pipeline

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,21 @@ output "instance_this_id" {
 }
 
 output "bucket_artifact_name" {
-  description = "The name of the S3 bucket"
-  value       = module.bucket_artifact.bucket_artifact_name
+  description = "The name of the S3 bucket with artifacts"
+  value       = module.bucket_artifact.bucket_name
+}
+
+output "bucket_frontend_name" {
+  description = "The name of the s3 bucket for frontend deployment"
+  value       = module.bucket_frontend.bucket_name
+}
+
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution"
+  value       = module.cloudfront.cloudfront_distribution_id
+}
+
+output "api_gateway_invoke_url" {
+  description = "Invoke URL for API Gateway"
+  value       = module.api_gateway.api_gateway_invoke_url
 }
